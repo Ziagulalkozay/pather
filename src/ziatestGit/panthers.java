1262f82805f9950cbd8ebe13296b7b0c501597f0
Ziagulalkozay <117605968+Ziagulalkozay@users.noreply.github.com>
2022-11-10 18:17:51 -0500

    Update panthers.java
    changes

--- src/ziatestGit/panthers.java
+++ src/ziatestGit/panthers.java
@@ -5,7 +5,8 @@ public class panthers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println(" This is the local test");
+		System.out.println(" This is the This is from remote test");
 	}
-	System.out.println(" This is the This is from remote test");
+	
 
 }
